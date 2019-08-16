1ab685180a798d8ef87216f9a609ba4d5d76cce6
Brian Dillingham <bdillingham88@gmail.com>
2019-08-16 19:29:16 -0400

    wip

--- src/NovaAssertions.php
+++ src/NovaAssertions.php
@@ -2,12 +2,16 @@
 
 namespace NovaTesting;
 
+use Illuminate\Support\Str;
 use NovaTesting\NovaResponse;
+use Illuminate\Database\Eloquent\Model;
 
 trait NovaAssertions
 {
     public function novaIndex($resource, $filters = [])
     {
+        $resource = $this->resolveResourceUriKey($resource);
+
         $filters = empty($filters) ? '' : $this->makeNovaFilters($resource, $filters);
 
         $json = $this->getJson("nova-api/$resource?$filters");
@@ -17,6 +21,8 @@ trait NovaAssertions
 
     public function novaDetail($resource, $resourceId)
     {
+        $resource = $this->resolveResourceUriKey($resource);
+
         $json = $this->getJson("nova-api/$resource/$resourceId");
 
         return new NovaResponse($json, compact('resource', 'resourceId'), $this);
@@ -24,6 +30,8 @@ trait NovaAssertions
 
     public function novaCreate($resource)
     {
+        $resource = $this->resolveResourceUriKey($resource);
+
         $json = $this->getJson("nova-api/$resource/creation-fields");
 
         return new NovaResponse($json, compact('resource'), $this);
@@ -31,6 +39,8 @@ trait NovaAssertions
 
     public function novaEdit($resource, $resourceId)
     {
+        $resource = $this->resolveResourceUriKey($resource);
+
         $json = $this->getJson("nova-api/$resource/$resourceId/update-fields");
 
         return new NovaResponse($json, compact('resource', 'resourceId'), $this);
@@ -45,5 +55,16 @@ trait NovaAssertions
         ));
 
         return "filters=$encoded";
+    }
+
+    public function resolveResourceUriKey($resource)
+    {
+        if (Str::contains('/', $resource)) {
+            abort_if(is_subclass_of(Model::class, $resource), 500, 'You used a Eloquent model vs a Nova resource');
+
+            return app($resource)->uriKey();
+        }
+
+        return $resource;
     }
 }
